78e70328ac7be32bc41a275f0bb6d4aea6878835
Hruaia <hruaitluangasailo20@gmail.com>
2026-02-26 20:37:49 +0530

    no

--- src/pages/Subjects.jsx
+++ src/pages/Subjects.jsx
@@ -64,7 +64,7 @@ export default function Subjects() {
                 key={item.id}
                 img="https://images.unsplash.com/photo-1513258496099-48168024aec0?w=600"
                 subject={item.name}
-                teacher={item.teacher_name}
+                teacher={item.teacher_names?.join(", ") || "No teacher assigned"}
                 onClick={() => navigate(`/subjects/${item.id}`)}
               />
             ))
